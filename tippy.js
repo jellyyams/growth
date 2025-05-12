081baa33c3d1562e11c821892a8df75e136628e0
jellyyams <53629007+jellyyams@users.noreply.github.com>
2025-05-12 19:57:40 +0300

    FIXED BUG YAYYYY

--- tippy.js
+++ tippy.js
@@ -1,10 +1,11 @@
 function setToolTips(){
-    n = svg.selectAll("circle"); 
+    n = svg.selectAll("g"); 
     n.attr("data-tippy-content", (d, i) => {
         let text =  "<b>" + d["Company"] + "</b>"; 
         text += ", founded in " + d["YearFounded"] + "," + d["Country"] + ". Acquired on " + d["Date"] + " for " + d["Cost"];
         return text;
     }); 
+
 
     tippy(n.nodes(), {
         allowHTML: true
