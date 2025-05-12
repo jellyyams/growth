70e4c2ab28463fb428f049728c8a4dd638058a14
jellyyams <53629007+jellyyams@users.noreply.github.com>
2025-05-12 22:39:28 +0300

    styling and coloring

--- tippy.js
+++ tippy.js
@@ -1,5 +1,7 @@
 function setToolTips(){
-    n = svg.selectAll("g"); 
+    n = svg.selectAll("g").filter(function(d){
+        return d["Company"] != "Microsoft Corp";
+    }); 
     n.attr("data-tippy-content", (d, i) => {
         let text =  "<b>" + d["Company"] + "</b>"; 
         text += ", founded in " + d["YearFounded"] + "," + d["Country"] + ". Acquired on " + d["Date"] + " for " + d["Cost"];
