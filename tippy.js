956b4fa1172963cc94483192ff11eb5caa878da2
jellyyams <53629007+jellyyams@users.noreply.github.com>
2025-05-26 11:56:05 +0300

    fixed tippy bug

--- tippy.js
+++ tippy.js
@@ -3,11 +3,13 @@ function setToolTips(){
         return d["Company"] != "Microsoft Corp";
     }); 
     n.attr("data-tippy-content", (d, i) => {
-        let text =  "<b>" + d["Company"] + "</b>"; 
-        text += ", founded in " + d["YearFounded"] + "," + d["Country"] + ". Acquired on " + d["Date"] + " for " + d["Cost"];
-        return text;
-    }); 
-
+        if(d["Company"] != "Microsoft Corp"){
+            let text =  "<b>" + d["Company"] + "</b>"; 
+            text += ", founded in " + d["YearFounded"] + "," + d["Country"] + ". Acquired on " + d["Date"] + " for " + d["Cost"];
+            return text;
+        }
+        
+    })
 
     tippy(n.nodes(), {
         allowHTML: true
