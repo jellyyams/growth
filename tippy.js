f17c95d8d7112089cb7743b70422648e7419720d
jellyyams <53629007+jellyyams@users.noreply.github.com>
2025-05-27 11:04:51 +0300

    add tippy log

--- tippy.js
+++ tippy.js
@@ -2,6 +2,7 @@ function setToolTips(){
     n = svg.selectAll("g").filter(function(d){
         return d["Company"] != "Microsoft Corp";
     }); 
+    console.log(n);
     n.attr("data-tippy-content", (d, i) => {
         if(d["Company"] != "Microsoft Corp"){
             let text =  "<b>" + d["Company"] + "</b>"; 
